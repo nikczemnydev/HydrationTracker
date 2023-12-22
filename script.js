81f16c4bd3b5f68b6259078d048554765f8ef12f
Rafal <136376818+nikczemnydev@users.noreply.github.com>
2023-12-22 19:49:44 +0100

    Added a method to fill the jug based on cups consumed. It now correctly shows the actual progress as well as remaining amount to reach the 2L goal.

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const percentage = document.getElementById('percentage');
 // grab element 'remaining'
 const remaining = document.getElementById('remaining');
 
+updateBigCup()
+
 // looping through small cups, getting indexes from 0 to 7
 smallCups.forEach((cup, index) => {
 // click event listener applies highlight function on clicked cups
@@ -30,4 +32,32 @@ function highlightCups(index) {
             cup.classList.remove('full')
         }
     })
+    updateBigCup()
+}
+
+// function that fills the jug based on glasses consumed
+function updateBigCup() {
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    const totalCups = smallCups.length
+
+    // no full cups = 0 intake so far = dont show percentage
+    if(fullCups === 0) {
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    } else {
+        // if any cups filled = show percentage
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+        percentage.innerText = `${fullCups / totalCups * 100}%`
+    }
+    // all cups full = daily intake goal reached = dont show percentage
+    if (fullCups === totalCups) {
+        remaining.style.visibility = 'hidden'
+        remaining.style.height = 0
+    } else {
+        // bring percentage and show litres remaining back 
+        // if set back to not full jug state
+        remaining.style.visibility = 'visible'
+        liters.innerText = `${2 - (250 * fullCups / 1000)}L`
+    }
 }
